730e69936898fcd320613cad352393682b88b99e
rastogi102 <nikhilrastogi102@gmail.com>
2023-07-26 14:58:11 +0530

    first commit

---
+++ BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+namespace ConAppAssignment16
+{
+    public class BankAccount
+    {
+        private static int nextAccountNumber = 01; // To automatically generate account numbers
+        private readonly int accountNumber; // Read-only property
+        private string accountHolderName;
+        private double balance;
+
+        // Properties
+        public int AccountNumber => accountNumber;
+        public string AccountHolderName
+        {
+            get => accountHolderName;
+            set => accountHolderName = value;
+        }
+
+        // Balance cannot be directly accessed; only through deposit and withdrawal methods
+        public double Balance => balance;
+
+        // Constructor
+        public BankAccount(string accountHolderName)
+        {
+            this.accountNumber = nextAccountNumber++;
+            this.AccountHolderName = accountHolderName;
+            this.balance = 0;
+        }
+
+        // Deposit method
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited: {amount:C}. Current balance: {balance:C}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount. Amount must be greater than zero.");
+            }
+        }
+
+        // Withdraw method
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawn: {amount:C}. Current balance: {balance:C}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid withdrawal amount. Amount must be greater than zero and less than or equal to the balance.");
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter account holder's name: ");
+            string accountHolderName = Console.ReadLine();
+
+            BankAccount account = new BankAccount(accountHolderName);
+
+            PrintAccountDetails(account);
+
+            // Deposit
+            Console.Write("Enter the amount to deposit: ");
+            if (double.TryParse(Console.ReadLine(), out double depositAmount))
+            {
+                account.Deposit(depositAmount);
+                PrintAccountDetails(account);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for deposit amount.");
+            }
+
+            // Withdraw
+            Console.Write("Enter the amount to withdraw: ");
+            if (double.TryParse(Console.ReadLine(), out double withdrawAmount))
+            {
+                account.Withdraw(withdrawAmount);
+                PrintAccountDetails(account);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for withdrawal amount.");
+            }
+            Console.ReadKey();
+
+        }
+
+        static void PrintAccountDetails(BankAccount account)
+        {
+            Console.WriteLine($"Account Number: {account.AccountNumber}, Account Holder: {account.AccountHolderName}, Balance: {account.Balance:C}");
+        }
+        
+    }
+}
+
